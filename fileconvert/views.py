106746464ea59f43dbd08f231155e7bc305f182d
user <user@example.com>
2024-08-14 20:39:00 +0900

    add textract receiver

--- fileconvert/views.py
+++ fileconvert/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 import logging
+import boto3
 
 
 # ログの名前空間取得
@@ -10,7 +11,43 @@ logger = logging.getLogger('general')
 # Convert画面表示
 #
 def index(request):
+
+    # ボタンが押下されたら実行
+    if "convert_progress" in request.POST:
+        # ログ出力
+        logger.info("Convertを開始します")
+
+        # ファイルをフォームから入手
+        fileObj = request.FILES['file']
+
+        # Textract送受信開始
+        resultTextract = textract_transceiver(fileObj)
+
+        # ログ出力
+        logger.info(resultTextract)
+
     # ログ出力
     logger.info("Convert画面を表示します")
     
     return render(request, 'convert.html')
+
+#
+# Textract送受信用関数
+#
+def textract_transceiver(uploadFiles):
+    # Amazon Textract client
+    textractClient = boto3.client('textract', region_name="ap-southeast-1")
+
+    # 画像ファイルを開く
+    # With文が終わるとファイルを閉じてメモリを解放する
+    with open(uploadFiles, 'rb') as file:
+        data = file.read()
+
+    # Amazon Textractを呼び出し、レスポンスをキャッチ
+    response = textractClient.detect_document_text(
+        Document={
+            'Bytes': data
+        }
+    )
+
+    return response
